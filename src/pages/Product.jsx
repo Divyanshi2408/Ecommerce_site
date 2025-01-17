a8e113aa93e8db582f9bad701b290c6fe4463e5b
Divyanshi <85859444+Divyanshi2408@users.noreply.github.com>
2025-01-17 21:49:15 +0530

    update

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProject from '../components/RelatedProject';
 
 const Product = () => {
   const {productId} = useParams();
@@ -57,14 +58,31 @@ const Product = () => {
           <p>Select Size</p>
           <div className='flex gap-2'>
             {productData.sizes.map((item,index)=>(
-              <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item=== size ? 'border-orange-500' : ' '}`} key={index}>{item}</button>
+              <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item=== size ? 'border-orange-500' : ' '}`} key={index}>{item}</button> 
             ))}
           </div>
         </div>
         <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-70'>ADD TO CART</button>
+        <hr className='mt-8 sm:w-4/5'/>
+              <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+              <p>100% Original product</p>
+              <p>Cash on delivery is available on this product</p>
+              <p>Easy retun and exchange policy within 7days</p>
+              </div>
       </div>
     </div>
-
+    <div className='mt-20'>
+    <div className='flex'>
+    <b className='border px-5 py-3 text-sm'>Description</b>
+    <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+    </div>
+    <div className='flex flex-col gap-4 border px-6 py-6 text-gray-500'>
+    <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
+    <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
+    </div>
+     </div>
+     
+<RelatedProject category={productData.category} subcategory={productData.subcategory}/>
     </div>
   ) :<div className='opacity-0'></div>
 }
